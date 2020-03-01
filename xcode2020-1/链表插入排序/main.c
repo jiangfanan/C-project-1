474720ebbdb3496c382bbb00077b73e775fc940c
jiangfanan <123@qq.com>
2020-03-01 14:51:55 +0800

    clean

--- xcode2020-1/链表插入排序/main.c
+++ xcode2020-1/链表插入排序/main.c
@@ -7,56 +7,30 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-struct node{
-    int data;
-    struct node *next;
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
 };
 
-int main() {
-    struct node *p;
-    struct node *head;
-    head =(struct node*)malloc (sizeof (struct node));
-    head->data=NULL;
-    head->next=NULL;//初始化头结点
-    int i=0;
-    int len=10;
-    srand(time(NULL));
-    for(i=0;i<len;i++){
-        p= (struct node *)malloc(sizeof(struct node));
-        p->data =rand()%100;
+typedef struct ListNode* LinkList;
 
-        p->next=head->next;
-        head->next=p;
-    }//利用头插法将数加入链表
-    struct node *q;
-    struct node *r;
-    p=head->next;
-    r=p;//r用来寻找每一次遍历的最小值
-    q=p->next;
-    while(p!=NULL){
-    while(q!=NULL){
-        if(q->data>p->data)
-            q=q->next;
-        if(q->next<=p->next)
-        {
-            r=q;
-            q=q->next;
+struct ListNode* insertionSortList(struct ListNode* head){
+    if(!head)
+        return NULL;
+    LinkList L=(LinkList )malloc(sizeof(struct ListNode));
+    L->next=head;
+    LinkList p=head->next,pre,temp;
+    head->next=NULL;
+    while(p){
+        temp=p->next;
+        pre=L;
+        while(pre->next&&pre->next->val<p->val){
+            pre=pre->next;
         }
+        p->next=pre->next;
+        pre->next=p;
+        p=temp;
     }
-     int m;
-     m=r->data;
-    r->data=p->data;
-    p->data=m;
-    p=p->next;
-        
-    }
-    while(p!=NULL){
-        printf("%d\n",p->data);
-       p=p->next;
-    }
-   return 0;
-    }
-
-
+    return L->next;
+}
